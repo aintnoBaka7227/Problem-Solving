5b9a39b4f94577697bd63e8a526200fecbcb7790
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-08-07 14:24:26 +0930

    final version

--- week2/ChangingString/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ChangingString.hpp"
-#include <iostream>
-
-int main() {
-    ChangingString test;
-    std::cout << test.distance("aa", "aa", 2);
-    return 0;
-}
--- week2/Reppity/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Reppity.hpp"
-#include <iostream>
-
-int main() {
-    Reppity test;
-    std::cout << test.longestRep("abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrstuvwxy");
-    return 0;
-}
