6208ec6ba693d0e89006ff6fb88f15a2767b2c0a
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-08-15 12:28:00 +0930

    so hard lol

--- week3/SimpleCompressor/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "SimpleCompressor.hpp"
-
-int main() {
-    SimpleCompressor test;
-    // std::cout << test.uncompress("[5[9CQKQYW][2[5QTYWYYND]]]") << std::endl;
-    test.uncompress("[5[9CQKQYW][2[5QTYWYYND]]]");
-    return 0;
-}
