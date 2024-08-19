42f4192fa5c9444d088aa4ab6b0f2dab1996f275
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-08-19 22:06:48 +0930

    demo

--- week4/UnsealTheSafe/main.cpp
+++ week4/UnsealTheSafe/main.cpp
@@ -2,7 +2,6 @@
 
 int main() {
     UnsealTheSafe test;
-    std::cout << test.countPasswords(2) << std::endl;
-    std::cout << test.countPasswords(3) << std::endl;
+    std::cout << test.countPasswords(20) << std::endl;
     return 0;
 }
