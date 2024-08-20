75232de541efa0c0339957bfc484f76630d589d3
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-08-20 22:26:06 +0930

    2

--- week5/RunLengthEncoding/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "RunLengthEncoding.hpp"
-
-int main() {
-    RunLengthEncoding test;
-    std::cout << test.decode("9999999999999999999999999999999999999999999999999X") << std::endl;
-    return 0;
-}
