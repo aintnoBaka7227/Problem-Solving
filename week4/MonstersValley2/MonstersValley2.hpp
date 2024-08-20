61ee33249762ff530a96f5dab67db68b9da96b3a
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-08-20 13:43:26 +0930

    demo

--- week4/MonstersValley2/MonstersValley2.hpp
+++ week4/MonstersValley2/MonstersValley2.hpp
@@ -5,7 +5,14 @@
 class MonstersValley2 {
     private:
     int backtracking(std::vector<int> dread, std::vector<int> price, int bribe, int strength, int index) {
-        
+        if (index == dread.size()) {
+            return bribe;
+        }
+        if (strength < dread[index]) {
+            return backtracking(dread, price, bribe + price[index], strength + dread[index], index + 1);
+        }
+        return std::min(backtracking(dread, price, bribe + price[index], strength + dread[index], index + 1), backtracking(dread, price, bribe, strength, index + 1));
+
     }
     public:
     int minimumPrice(std::vector<int> dread, std::vector<int> price) {
---
+++ week4/MonstersValley2/main.cpp
@@ -0,0 +1,7 @@
+#include "MonstersValley2.hpp"
+
+int main() {
+    MonstersValley2 test;
+    std::cout << test.minimumPrice({5216, 12512, 613, 1256, 66, 17202, 30000, 23512, 2125, 33333},{2, 2, 1, 1, 1, 1, 2, 1, 2, 1}) << std::endl;
+    return 0;
+}
