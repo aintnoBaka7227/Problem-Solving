85fe672784f76d8e181ade83d616048ddeeed0ed
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-08-20 21:22:50 +0930

    demo

--- week5/RunLengthEncoding/RunLengthEncoding.hpp
+++ week5/RunLengthEncoding/RunLengthEncoding.hpp
@@ -7,15 +7,15 @@ class RunLengthEncoding {
     std::string decode(std::string text) {
         std::string decoded_text;
         for (int i = 0; i < text.length(); i++) {
-            std::cout << "i: " << i << std::endl;
+            //std::cout << "i: " << i << std::endl;
             std::string count;
             std::string decode;
             while (text[i] - '0' >= 0 && text[i] - '0' <= 9) {
                 count+=text[i];
                 i++;
-                std::cout << "new i: " << i << std::endl;
+                //std::cout << "new i: " << i << std::endl;
             }
-            std::cout << "new count: " << count << std::endl;
+            //std::cout << "new count: " << count << std::endl;
             if (count != "") {
                 for (int j = 0; j < std::stoi(count); j++) {
                     decode += text[i]; 
@@ -25,8 +25,8 @@ class RunLengthEncoding {
             else {
                 decoded_text +=text[i];
             }
-            std::cout << "decode: " << decode << std::endl;
-            std::cout << "decoded_text: " << decoded_text << std::endl;
+            //std::cout << "decode: " << decode << std::endl;
+            //std::cout << "decoded_text: " << decoded_text << std::endl;
         }
         
         if (decoded_text.length() > 50) {
