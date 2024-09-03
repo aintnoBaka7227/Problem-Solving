09d2d41da6aaf52288b388b1d4eaccd96c9aa185
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-09-03 15:30:27 +0930

    debug

--- week7/PrefixFreeSets/PrefixFreeSets.hpp
+++ week7/PrefixFreeSets/PrefixFreeSets.hpp
@@ -3,23 +3,39 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <set>
 
 class PrefixFreeSets {
     public:
     int maxElements(std::vector<std::string> words) {
-        std::vector<int> results;
-        for (int i = 0; i < words.size(); i++) {
-            int result = words.size();
-            for (int j = 0; j < words.size(); j++) {
+        std::vector<bool> results;
+        std::set<std::string> unique_words;
+        for (auto word : words) {
+            unique_words.insert(word);
+        }
+        std::vector<std::string> unique;
+        for (auto w : unique_words) {
+            unique.push_back(w);
+        }
+        //std::cout << unique.size() << std::endl;
+        for (int i = 0; i < unique.size(); i++) {
+            int result = 0;
+            for (int j = 0; j < unique.size(); j++) {
                 if (i != j) {
-                    if (words[i].find(words[j]) == 0 || words[j].find(words[i]) == 0) { 
-                        result--;
+                    if (unique[i].length() <= unique[j].length()) { 
+                        if (unique[j].substr(0, unique[i].length()) == unique[i]){
+                            result = 1;
+                        }
                     }
                 }
             }
             results.push_back(result);
         }
-        std::sort(results.begin(), results.end());
-        return results[0];
+        int count = 0;
+        for (auto result : results) {
+            std::cout << result << std::endl;
+            if (!result) count++;
+        }
+        return count;
     }
 };
--- week7/PrefixFreeSets/main.cpp
+++ week7/PrefixFreeSets/main.cpp
@@ -2,6 +2,29 @@
 
 int main() {
     PrefixFreeSets test;
-    std::cout << test.maxElements({"topcoder","topcoder","topcoding"}) << std::endl;
+    std::vector<std::string> words = {
+        "svvdxggvhraktqqkkfl", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsogvobw", "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdldp", 
+        "xgejpsemtuhshxpziqviiqs", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsogvobw", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsogvobw", 
+        "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekf", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsog", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsogvobw", 
+        "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdldn", "eblhpwsvbgjskhfdsqmdsxxhupcydgroae", "eonijaazhtdnvwryvjdcndteoeqxncqhddqofzcus", 
+        "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdldp", "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekfsc", "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekfsc", 
+        "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdldp", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxdr", 
+        "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxdr", "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdldn", 
+        "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekfscd", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxd", 
+        "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsogvobw", "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdldp", 
+        "hfolqqhfvrfzdgsuuni", "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdldn", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxdr", 
+        "fynrfcnxfjmqswmbwmznfzvaeocdozyorbfvcrguygztojdld", "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekfscd", 
+        "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxd", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxdr", 
+        "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvb", "axcm", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxdr", 
+        "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvs", "anlecahk", "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekfscd", 
+        "eonijaazhtdnvwryvjdcndteoeqxncqhddqofzc", "eonijaazhtdnv", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxdr", 
+        "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxdr", "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvso", 
+        "anlecahkrizwtqobdnycxeocojpumjfqtmpavuanhwvsjvbxd", "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekfscg", 
+        "eonijaazhtdnvcwamgwulzwwuoypkqxaxzuttvbvsot", "eonijaazhtdnvwryvjdcndteoeqxncqhddq", 
+        "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibes", "hfolqqhfvrfzdgsuuniyakyrottbfgtouxbfcibesuoqekfscd", 
+        "eonijaazhtdnvpwxyeaprmzuscqdqjvs", "eonijaazhtdn", "fynrfcnxfjmqswmbwmznfzvae"
+    };
+    std::cout << test.maxElements(words) << std::endl;
+    // std::cout << test.maxElements({"hello","hi","h","run","rerun","running"}) << std::endl;
     return 0;
 }
