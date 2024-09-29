648e93585004a67c17672dbbd5b5f9d9c129b972
aintnoBaka7227 <phongtrando2004@gmail.com>
2024-09-29 21:41:14 +0930

    demo memoization

--- week8/HandsShaking/HandsShaking.hpp
+++ week8/HandsShaking/HandsShaking.hpp
@@ -1,9 +1,16 @@
 #pragma once
 #include <iostream>
+#include <vector>
 
 class HandsShaking {
     public:
-    long countPerfect(int n) {
+    std::vector<long> counts;
+
+    long recursiveCount(int n) {
+        if (counts[n] != 0) {
+            return counts[n]; 
+        }
+
         if (n==2 || n==0) {
             return 1; 
         }
@@ -11,9 +18,17 @@ class HandsShaking {
         long result = 0; 
 
         for (int i = 2; i <= n; i+=2) {
-            result += countPerfect(i-2)*countPerfect(n-i);
+            result += recursiveCount(i-2)*recursiveCount(n-i);
         }
 
+        counts[n] = result;
         return result; 
+    }
+
+    long countPerfect(int n) {
+        for (int i = 0; i < 51; i++) {
+            counts.push_back(0);
+        }
+        return recursiveCount(n);
     }
 };
